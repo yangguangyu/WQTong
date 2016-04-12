85faabc6bf903b694530c19596584ad97d08e8cd
cnbin <845040571@qq.com>
2016-04-12 15:40:36 +0800

    change website

--- WQTong/GlobalResource/GlobalResource.h
+++ WQTong/GlobalResource/GlobalResource.h
@@ -9,7 +9,6 @@
 #import <Foundation/Foundation.h>
 #import "WzcLocObjectModel.h"
 
-
 @class WzcLocObjectModel;
 
 @interface GlobalResource : NSObject
@@ -21,15 +20,5 @@
 
 //按日期查询到的WzcLocObjectModel
 @property (strong, nonatomic) WzcLocObjectModel *detailResultRunObject;
-
-//@property (strong, nonatomic) NSString * wz;
-
-////每15分钟的位置的经纬度
-//@property (nonatomic, assign) CGFloat searchLatitude;
-//@property (nonatomic, assign) CGFloat searchLongitude;
-//
-////上下班打卡经纬度
-//@property (nonatomic, assign) NSNumber *reportlongitudeX; //经度
-//@property (nonatomic, assign) NSNumber *reportLatitudeY;  //纬度
 
 @end
--- WQTong/GlobalResource/GlobalResource.m
+++ WQTong/GlobalResource/GlobalResource.m
@@ -10,15 +10,62 @@
 
 @implementation GlobalResource
 
+//一般单例模式
+//__strong static GlobalResource *share = nil;
+//
+//+ (instancetype)sharedInstance
+//{
+//    static dispatch_once_t pred = 0;
+//    dispatch_once(&pred, ^{
+//        share = [[super allocWithZone:NULL] init];
+//    });
+//    return share;
+//}
+
+//严格单例模式，防止子类使用
 __strong static GlobalResource *share = nil;
 
-+ (instancetype)sharedInstance
-{
-    static dispatch_once_t pred = 0;
-    dispatch_once(&pred, ^{
-        share = [[super allocWithZone:NULL] init];
++ (instancetype)sharedInstance {
+    
+    static dispatch_once_t predicate;
+    dispatch_once(&predicate, ^{
+        
+        share = (GlobalResource *)@"GlobalResource";
+        share = [[GlobalResource alloc] init];
     });
+    
+    // 防止子类使用
+    NSString *classString = NSStringFromClass([self class]);
+    if ([classString isEqualToString:@"GlobalResource"] == NO) {
+        
+        NSParameterAssert(nil);
+    }
+    
     return share;
+}
+
+- (instancetype)init {
+    
+    NSString *string = (NSString *)share;
+    if ([string isKindOfClass:[NSString class]] == YES && [string isEqualToString:@"GlobalResource"]) {
+        
+        self = [super init];
+        if (self) {
+            
+            // 防止子类使用
+            NSString *classString = NSStringFromClass([self class]);
+            if ([classString isEqualToString:@"GlobalResource"] == NO) {
+                
+                NSParameterAssert(nil);
+            }
+        }
+        
+        return self;
+        
+    } else {
+        
+        return nil;
+    }
 }
 
 @end
